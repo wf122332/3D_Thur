c01575b5989f483e382849447a8b4d0c4dcd05f5
wf122332 <76959987+wf122332@users.noreply.github.com>
2022-12-14 14:49:04 +0800

    update: 完成12.3基本對話介面

---
+++ 3D_Thur/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+
+    [CreateAssetMenu(menuName ="S/Dialogue Data",fileName ="New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話者內容"), TextArea(2, 10)]
+        public string[] dialogueContents;
+    }
+}
