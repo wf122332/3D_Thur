f455af6670462add7903c8780baa124ad9b1ecef
wf122332 <76959987+wf122332@users.noreply.github.com>
2022-12-15 13:59:16 +0800

    update:完成課程13

--- 3D_Thur/Assets/Scripts/DialogueSystem.cs
+++ 3D_Thur/Assets/Scripts/DialogueSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 namespace S
 {
@@ -27,8 +28,37 @@ namespace S
             textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
             goTriangle = GameObject.Find("對話完成圖示");
             goTriangle.SetActive(false);
+
+            StartCoroutine(FadeGroup());
+            StartCoroutine(TypeEffect());
         }
 
+        /// <summary>
+        /// 淡入淡出群組物件
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator FadeGroup()
+        {
+            for(int i=0;i<10;i++)
+            {
+                groupDialogue.alpha += 0.1f;
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
 
+        private IEnumerator TypeEffect()
+        {
+            textName.text = dialogueOpening.dialogueName;
+            textContent.text = "";
+
+            string dialogue = dialogueOpening.dialogueContents[1];
+
+            for(int i=0;i<dialogue.Length;i++)
+            {
+                textContent.text += dialogue[i];
+                yield return dialogueInterval;
+            }
+            goTriangle.SetActive(true);
+        }
     }
 }
